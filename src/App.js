4b6c1fd9bbd7a763cd462e81206a00341505ec69
PedroLucasSS <pedro.lucas4431@gmail.com>
2020-06-16 20:51:16 -0300

    finalizando funcionalidades

--- src/App.js
+++ src/App.js
@@ -1,29 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Form } from '@unform/web'
 
+import api from './services/api'
+
+import Input from './components/Form/Input'
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+
+  const [ repositories, setRepositories ] = useState([])
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
+  async function handleAddRepository(data) {
+    const newRepository = await api.post('/repositories', {
+      title: `${data.repository}`,
+    })
+
+    if(newRepository.data.title == ''){
+      alert('Digite um repositório')
+      return
+    }
+
+    setRepositories([...repositories, newRepository.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`)
+
+    const newRepository = repositories.filter(repository => {
+      if(repository.id != id) { return repository }
+    })
+
+    setRepositories(newRepository)
   }
 
   return (
     <div>
+      <h1>Desafio: Conceitos do ReactJS</h1>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        { 
+          repositories.map(repository => (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                  Remover
+              </button>
+            </li>
+          ))
+        }
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <Form onSubmit={handleAddRepository}>
+        <Input 
+          name="repository" 
+          type="text" 
+          id="repo" 
+          placeholder="Digite um repositório"
+        />
+        <button type="submit" >Adicionar</button>
+      </Form>
     </div>
   );
 }
